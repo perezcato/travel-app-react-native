2c9154f0995d33a60383da42e52c94b696874e45
jonspiro <jonspiro@icloud.com>
2020-10-08 16:27:39 +0000

    added the fonts and colors to the project

---
+++ src/config/colors.js
@@ -0,0 +1,9 @@
+const colors = {
+  PRIMARY_COLOR: '#1b44a6',
+  SECONDARY_COLOR: '#84d2fa',
+  SECONDARY_BLUE: '#0b3c95',
+  YELLOW: '#fdd231',
+  WHITE: '#ffffff',
+};
+
+export default colors;
---
+++ src/config/fonts.js
@@ -0,0 +1,19 @@
+const fonts = {
+  BLACK: 'HelveticaNeueCyr-Black',
+  BLACK_ITALIC: 'HelveticaNeueCyr-BlackItalic',
+  BOLD: 'HelveticaNeueCyr-Bold',
+  BOLD_ITALIC: 'HelveticaNeueCyr-BoldItalic',
+  HEAVY: 'HelveticaNeueCyr-Heavy',
+  HEAVY_ITALIC: 'HelveticaNeueCyr-HeavyItalic',
+  ITALIC: 'HelveticaNeueCyr-Italic',
+  LIGHT: 'HelveticaNeueCyr-Light',
+  LIGHT_ITALIC: 'HelveticaNeueCyr-LightItalic',
+  MEDIUM: 'HelveticaNeueCyr-Medium',
+  ROMAN: 'HelveticaNeueCyr-Roman',
+  THIN: 'HelveticaNeueCyr-Thin',
+  THIN_ITALIC: 'HelveticaNeueCyr-ThinItalic',
+  ULTRA_LIGHT: 'HelveticaNeueCyr-UltraLight',
+  ULTRA_LIGHT_ITALIC: 'HelveticaNeueCyr-UltraLightItalic',
+};
+
+export default fonts;
